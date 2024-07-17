7ba8f51f0dc2b79b17b59d986ef463d7a7dbda50
FrenzyAsWell <Frenzy4Way@gmail.com>
2024-07-17 19:31:03 +0300

    ADD_FEATURE store data in 2D array

--- src/main.c
+++ src/main.c
@@ -4,41 +4,62 @@
 
 #define ARG_INPUT_COMMAND 2
 
-char buffer[512];
+int iArrSize = 256;
+char buffer[256];
+
+char** GetOptions(char* pCommand);
+void EraseData(char** arrErasable);
 
 int main(int argc, char *argv[])
 {
-	char *pCommand;
-	char pNewCommand[256];
-	FILE *fpHelp;
-
 	if (argc != ARG_INPUT_COMMAND) 
 	{
 		printf("Help option");
 		return 1;	
 	}
 	
-	pCommand = argv[argc - 1];
+	char** arrOptions = GetOptions(argv[argc - 1]);
+
+	EraseData(arrOptions);
+
+	return 0;
+}
+
+char** GetOptions(char* pCommand)
+{	
+	char pNewCommand[256];
+	FILE *fpHelp;
+
+	char** arrOptions = malloc(iArrSize * sizeof(char*));
+
 	strcat(pCommand, " --help");
 
 	fpHelp = popen(pCommand, "r");
-	while (fgets(buffer, sizeof(buffer), fpHelp) != NULL)
+	for (int a = 0; fgets(buffer, sizeof(buffer), fpHelp) != NULL; a++)
 	{
 		char *pNew = strstr(buffer, "--");
 		if (pNew == NULL) 
 			continue;
 			
-		memmove(buffer, pNew + 1, strlen(pNew + 1) + 1);
-		for (int a = 0; a < strlen(buffer) + 1; a++) 
+		memmove(buffer, pNew + 2, strlen(pNew + 1) + 1);
+		for (int b = 0; b < strlen(buffer) + 1; b++) 
 		{
-			if (buffer[a] == '=' || buffer[a] == ' ')
+			if (buffer[b] == '=' || buffer[b] == ' ')
 				break;
-			pNewCommand[a] = buffer[a];
+			pNewCommand[b] = buffer[b];
 		}
 
-		printf("%s\n", pNewCommand);
-		memset(pNewCommand, '\0', sizeof(pNewCommand));
+		arrOptions[a] = malloc(strlen(pNewCommand) * sizeof(char));
+		strcpy(arrOptions[a], pNewCommand);
 	}
 
-	return 0;
+	return arrOptions;
+}
+
+void EraseData(char** arrErasable)
+{
+	for (int a = 0; a < iArrSize; a++)
+		if (arrErasable[a] != NULL) 
+			free(arrErasable[a]);
+	free(arrErasable);
 }
