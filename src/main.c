667d6abbb05e4fc2bf9f62cc9a13a8432a8b49de
FrenzyAsWell <Frenzy4Way@gmail.com>
2024-07-26 19:36:55 +0300

    ADD ui update && flag selection

--- src/main.c
+++ src/main.c
@@ -13,15 +13,25 @@ typedef struct
 } stWindowSize;
 
 typedef struct {
+	int* list_selected;
+	int count_selected;
+} stSelection;
+
+typedef struct {
 	WINDOW* hFlags;
 	char** name_flag;
 
+	stSelection object_selection;
+
 	int offset;
+	char* window_name;
 } stFlags;
 
 typedef struct {
 	WINDOW* hDescriptions;
 	char** text_description;
+
+	char* window_name;
 } stDescription;
 
 typedef struct {
@@ -37,7 +47,8 @@ char updated_buffer[256];
 void InteractData(stMainWindow arrErasable);
 void GetOptions(stMainWindow* arrOptions, char* pCommand);
 void EraseStruct(stMainWindow arrErasable);
-void PrintData(WINDOW* wndMain, char** arrPrintable, int iSize, int iHighlighted, int* iyOffset);
+void PrintWindowData(WINDOW* wndMain, char** arrPrintable, int iSize, int iHighlighted, stSelection stSelection, int* iyOffset, char* sWindowName);
+void AddSelection(stSelection* object_selection, int iHighlighted);
 stWindowSize GetFitSize(char** arrData, int iSize);
 
 int main(int argc, char *argv[])
@@ -52,6 +63,7 @@ int main(int argc, char *argv[])
 	GetOptions(&arrData, argv[argc - 1]);
 
 	initscr();
+	start_color();
 	curs_set(0);
 	noecho();
 
@@ -82,6 +94,7 @@ void GetOptions(stMainWindow* arrOptions, char* pCommand)
 	}
 
 	(*arrOptions).wndFlags.name_flag = malloc(iLines * sizeof(char*));
+	(*arrOptions).wndDescription.text_description = malloc(iLines * sizeof(char*));
 
 	fpHelp = popen(pCommand, "r");
 	for (int a = 0; fgets(buffer, sizeof(buffer), fpHelp) != NULL; a++)
@@ -110,11 +123,10 @@ void GetOptions(stMainWindow* arrOptions, char* pCommand)
 		(*arrOptions).wndFlags.name_flag[a] = malloc((strlen(pNewFlag) + 1) * sizeof(char*));
 		strcpy((*arrOptions).wndFlags.name_flag[a], pNewFlag);
 
-		/*
 		memset(updated_buffer, '\0', sizeof(updated_buffer));
-		for (int b = iPosSymbol, c = 0; b < strlen(buffer) + 1; b++) 
+		for (int b = iPosSymbol + 1, c = 0; b < strlen(buffer) + 1; b++) 
 		{
-			if (buffer[b] == '\n')
+			if (buffer[b] == '\n' || (buffer[b - 1] == ' ' && buffer[b] == ' '))
 				continue;
 
 			updated_buffer[c] = buffer[b];
@@ -124,11 +136,10 @@ void GetOptions(stMainWindow* arrOptions, char* pCommand)
 
 		(*arrOptions).wndDescription.text_description[a] = malloc((strlen(pNewDescription) + 1) * sizeof(char*));
 		strcpy((*arrOptions).wndDescription.text_description[a], pNewDescription);
-		*/
 	}
 
 	arrOptions->count_options = iLines;
-	//fclose(fpHelp);
+	fclose(fpHelp);
 }
 
 stWindowSize GetFitSize(char** arrData, int iSize) 
@@ -146,45 +157,115 @@ stWindowSize GetFitSize(char** arrData, int iSize)
 	return twndSize;
 }
 
-void PrintData(WINDOW* wndMain, char** arrPrintable, int iSize, int iHighlighted, int* iyOffset)
+void AddSelection(stSelection* object_selection, int iHighlighted)
+{
+	bool isSelected = false;
+	int index = 0;
+
+	for (int a = 0; a < object_selection->count_selected; a++)
+		if (object_selection->list_selected[a] == iHighlighted) 
+		{
+			isSelected = true;
+			index = a;
+
+			break;
+		}
+
+	if (isSelected == true) 
+	{
+		for (int b = index; b < object_selection->count_selected; b++)
+        	object_selection->list_selected[b] = object_selection->list_selected[b + 1];
+	}
+	else
+	{
+		object_selection->list_selected = realloc(object_selection->list_selected, (object_selection->count_selected + 1) * sizeof(int));
+
+		object_selection->list_selected[object_selection->count_selected] = iHighlighted;
+		object_selection->count_selected++;
+	}
+}
+
+void PrintWindowData(WINDOW* wndMain, char** arrPrintable, int iSize, int iHighlighted, stSelection stSelection, int* iyOffset, char* sWindowName)
 { 
 	int yMax = getmaxy(wndMain);
 	if (iHighlighted > getmaxy(wndMain) - 3)
 		*iyOffset = iHighlighted - (getmaxy(wndMain) - 3);
 
-	refresh();
-	for (int a = 0, b = *iyOffset; b < iSize; a++, b++)
-	{
-		if (b == iHighlighted)
-			wattron(wndMain, A_REVERSE);
+	if (iSize > 1)
+		for (int a = 0, b = *iyOffset; b < iSize; a++, b++)
+		{
+			for (int c = 0; c < stSelection.count_selected; c++) 
+				if (stSelection.list_selected[c] == b) 
+				{
+					init_pair(1, COLOR_GREEN, 0);
+					wattron(wndMain, COLOR_PAIR(1));
 
-		mvwprintw(wndMain, a + 1, 1, "%s\n", arrPrintable[b]);
-		refresh();
+					break;
+				}
 
-		wattroff(wndMain, A_REVERSE);
-	}
+			if (b == iHighlighted)
+				wattron(wndMain, A_REVERSE);
+
+			mvwprintw(wndMain, a + 1, 1, "%s\n", arrPrintable[b]);
+			refresh();
+
+			wattroff(wndMain, A_REVERSE);
+			wattroff(wndMain, COLOR_PAIR(1));
+		}
+	else
+		if (arrPrintable != NULL)
+		{
+			werase(wndMain);
+			mvwprintw(wndMain, 1, 1, "%s", arrPrintable[iHighlighted]);
+		}
+
 
 	box(wndMain, 0, 0);
+
+	mvwprintw(wndMain, 0, 2, " %s ", sWindowName);
 	wrefresh(wndMain);
 }
 
 void InteractData(stMainWindow arrPrintable)
 {
 	int iInterface = 0;
-	int iHighlighted = -1;
+	int iHighlighted = 0;
 
 	int ixMax, iyMax;
 	getmaxyx(stdscr, iyMax, ixMax);
 
 	stWindowSize twndOptionSize = GetFitSize(arrPrintable.wndFlags.name_flag, arrPrintable.count_options);
+	twndOptionSize.x_size += 10;
+
 	arrPrintable.wndFlags.hFlags = newwin(getmaxy(stdscr), twndOptionSize.x_size, 0, 0);
+	arrPrintable.wndFlags.window_name = "Flags";
 	
+	arrPrintable.wndDescription.hDescriptions = newwin(getmaxy(stdscr), getmaxx(stdscr) - twndOptionSize.x_size, 0, twndOptionSize.x_size);
+	arrPrintable.wndDescription.window_name = "Description";
+
 	keypad(arrPrintable.wndFlags.hFlags, true);
+
+	arrPrintable.wndFlags.object_selection.count_selected = 0;
+	arrPrintable.wndFlags.object_selection.list_selected = malloc(arrPrintable.wndFlags.object_selection.count_selected * sizeof(int));
 
 	arrPrintable.wndFlags.offset = 0;
 	while (1) 
 	{
-		PrintData(arrPrintable.wndFlags.hFlags, arrPrintable.wndFlags.name_flag, arrPrintable.count_options, iHighlighted, &arrPrintable.wndFlags.offset);
+		PrintWindowData(arrPrintable.wndFlags.hFlags, 
+						arrPrintable.wndFlags.name_flag, 
+						arrPrintable.count_options, 
+						iHighlighted, 
+						arrPrintable.wndFlags.object_selection,
+						&arrPrintable.wndFlags.offset, 
+						arrPrintable.wndFlags.window_name);
+
+		PrintWindowData(arrPrintable.wndDescription.hDescriptions, 
+						arrPrintable.wndDescription.text_description, 
+						1, 
+						iHighlighted, 
+						arrPrintable.wndFlags.object_selection,
+						&arrPrintable.wndFlags.offset, 
+						arrPrintable.wndDescription.window_name);
 
 		iInterface = wgetch(arrPrintable.wndFlags.hFlags);
 		switch (iInterface) 
@@ -203,6 +284,12 @@ void InteractData(stMainWindow arrPrintable)
 
 				if (iHighlighted > arrPrintable.count_options - 1)
 					iHighlighted = arrPrintable.count_options - 1;
+
+				break;
+			}
+			case 10:
+			{
+				AddSelection(&arrPrintable.wndFlags.object_selection, iHighlighted);
 
 				break;
 			}
