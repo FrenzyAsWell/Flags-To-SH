f0ec35f7e0d535f64027129aa7c7bbf9c5939cd2
FrenzyAsWell <Frenzy4Way@gmail.com>
2024-08-29 18:01:24 +0300

    INIT FXTUI branch

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,13 +1,15 @@
+#include <iostream>
+#include <string>
+
 #include <ftxui/component/component_options.hpp>
 #include <ftxui/component/event.hpp>
 #include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/component/component.hpp>
+
 #include <ftxui/dom/direction.hpp>
-#include <iostream>
+#include <ftxui/dom/elements.hpp>
 
 #include <ftxui/screen/screen.hpp>
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/component/component.hpp>
-#include <string>
 
 int main()
 {
